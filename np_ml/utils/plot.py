f117937dfc6317566c452395161e420bf2d9c5f1
fengjiran <fengjiran@foxmail.com>
2018-12-05 17:14:59 +0800

    plot boundary

--- np_ml/utils/plot.py
+++ np_ml/utils/plot.py
@@ -16,3 +16,36 @@ def plot(x, y, **kwargs):
     if 'accuracy' in kwargs:
         plt.title("Accuracy: %.1f%%" % (kwargs['accuracy'] * 100), fontsize=10)
     plt.show()
+
+
+def plot_boundary(model, x, y, **kwargs):
+    assert (x.shape[-1] == 2)
+    cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+    cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+    if 'h' in kwargs:
+        h = kwargs['h']
+    else:
+        h = 0.1
+
+    x_min, x_max = x[:, 0].min() - 1, x[:, 0].max() + 1
+    y_min, y_max = x[:, 1].min() - 1, x[:, 1].max() + 1
+    x_grid, y_grid = np.meshgrid(np.arange(x_min, x_max, h),
+                                 np.arange(y_min, y_max, h))
+    Z = model.predict(np.c_[x_grid.ravel(), y_grid.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(x_grid.shape)
+    plt.figure()
+    plt.pcolormesh(x_grid, y_grid, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    plt.scatter(x[:, 0], x[:, 1], c=y, cmap=cmap_bold,
+                edgecolor='k', s=20)
+    plt.xlim(x_grid.min(), x_grid.max())
+    plt.ylim(y_grid.min(), y_grid.max())
+
+    if 'title' in kwargs:
+        plt.suptitle(kwargs['title'])
+    if 'accuracy' in kwargs:
+        plt.title("Accuracy: %.1f%%" % (kwargs['accuracy'] * 100), fontsize=10)
+    plt.show()

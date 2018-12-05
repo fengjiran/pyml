74da44b554fae9e4fb357222094404593822c211
fengjiran <fengjiran@foxmail.com>
2018-12-05 17:07:11 +0800

    assert

--- np_ml/utils/plot.py
+++ np_ml/utils/plot.py
@@ -1,3 +1,9 @@
 import matplotlib as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
+
+
+def plot(x, y, **kwargs):
+    """can only do 2D plot right now"""
+    assert (x.shape[-1] == 2)
+    color = (y + 2) / 5

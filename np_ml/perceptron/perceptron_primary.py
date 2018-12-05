90b88fdd000d72b797268896be590a5716535cbd
fengjiran <fengjiran@foxmail.com>
2018-12-05 22:37:48 +0800

    fit function

--- np_ml/perceptron/perceptron_primary.py
+++ np_ml/perceptron/perceptron_primary.py
@@ -6,4 +6,11 @@ import numpy.linalg as LA
 
 class Perceptron(object):
     def __init__(self, dim=2, eta=1, max_epoch=None):
+        self.dim = dim
+        self.eta = eta
+        self.W = np.zeros(dim)
+        self.b = np.zeros(1)
+        self.max_epoch = 1000
+
+    def fit(self, x, y, detailed=False):
         pass

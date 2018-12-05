60fab24057cede5b56f352c2e4136a85dbcf5e89
fengjiran <fengjiran@foxmail.com>
2018-12-05 17:09:19 +0800

    plt

--- np_ml/utils/plot.py
+++ np_ml/utils/plot.py
@@ -7,3 +7,7 @@ def plot(x, y, **kwargs):
     """can only do 2D plot right now"""
     assert (x.shape[-1] == 2)
     color = (y + 2) / 5
+    if 'accuracy' in kwargs:
+        accuracy = kwargs['accuracy']
+    plt.figure()
+    plt.scatter(x[:, 0], x[:, 1], c=color)
